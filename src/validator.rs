7d65e6c21af67376e3f6d1b8d62fea1e788fb79e
Ekojoecovenant <covenantekojoe@gmail.com>
2026-03-21 16:36:09 +0000

    refactor: add impl block to ValidationError

--- src/validator.rs
+++ src/validator.rs
@@ -2,19 +2,24 @@ use std::collections::HashMap;
 
 pub struct ValidationError {
     pub key: String,
-    pub _rule: String,
+    pub rule: String,
     pub message: String,
 }
 
+impl ValidationError {
+    fn new(key: String, rule: String, message: String) -> Self {
+        ValidationError { key, rule, message }
+    }
+}
+
 pub fn validate_env(map: HashMap<String, Option<String>>) -> Vec<ValidationError> {
-    // let jwt_val = map.get("JWT_SECRET");
     let mut vec_errors: Vec<ValidationError> = Vec::new();
 
     for (key, value) in map {
         if key.contains("SECRET") {
             match value {
                 None => {
-                    vec_errors.push(error_gen(
+                    vec_errors.push(ValidationError::new(
                         key,
                         "empty".to_string(),
                         "must not be empty".to_string(),
@@ -23,7 +28,7 @@ pub fn validate_env(map: HashMap<String, Option<String>>) -> Vec<ValidationError
                 Some(val) => {
                     let is_valid = val.chars().count() >= 32;
                     if !is_valid {
-                        vec_errors.push(error_gen(
+                        vec_errors.push(ValidationError::new(
                             key,
                             "min_length".to_string(),
                             "must be greater than or equal to 32".to_string(),
@@ -34,7 +39,7 @@ pub fn validate_env(map: HashMap<String, Option<String>>) -> Vec<ValidationError
         } else if key.contains("PORT") {
             match value {
                 None => {
-                    vec_errors.push(error_gen(
+                    vec_errors.push(ValidationError::new(
                         key,
                         "empty".to_string(),
                         "must not be empty".to_string(),
@@ -43,7 +48,7 @@ pub fn validate_env(map: HashMap<String, Option<String>>) -> Vec<ValidationError
                 Some(val) => {
                     let is_num = val.parse::<u16>().is_ok();
                     if !is_num {
-                        vec_errors.push(error_gen(
+                        vec_errors.push(ValidationError::new(
                             key,
                             "format".to_string(),
                             "must be a number".to_string(),
@@ -54,24 +59,19 @@ pub fn validate_env(map: HashMap<String, Option<String>>) -> Vec<ValidationError
         } else if key.contains("URL") {
             match value {
                 None => {
-                    vec_errors.push(error_gen(
+                    vec_errors.push(ValidationError::new(
                         key,
                         "empty".to_string(),
                         "must not be empty".to_string(),
                     ));
                 }
                 Some(val) => {
-                    let is_valid = if val.starts_with("http://")
+                    let is_valid = val.starts_with("http://")
                         || val.starts_with("https://")
                         || val.starts_with("postgres://")
-                        || val.starts_with("mysql://")
-                    {
-                        true
-                    } else {
-                        false
-                    };
+                        || val.starts_with("mysql://");
                     if !is_valid {
-                        vec_errors.push(error_gen(
+                        vec_errors.push(ValidationError::new(
                             key,
                             "format".to_string(),
                             "must start with http://, https://, postgres://, mysql://".to_string(),
@@ -83,12 +83,4 @@ pub fn validate_env(map: HashMap<String, Option<String>>) -> Vec<ValidationError
     }
 
     vec_errors
-}
-
-fn error_gen(key: String, _rule: String, message: String) -> ValidationError {
-    ValidationError {
-        key,
-        _rule,
-        message,
-    }
 }
